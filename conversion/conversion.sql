94853537615876be00f78e085ee85d70564573bc
michaelwestblade <michaelwestblade@gmail.com>
2025-10-25 16:50:05 -0500

    add SQL conversion examples and update IDE config files

---
+++ conversion/conversion.sql
@@ -0,0 +1,15 @@
+select *
+from movies where id = integer '1';
+
+select cast('1' as integer);
+select cast('10' as float);
+
+select cast('2025-01-01' as date);
+select cast('2025-01-01 12:00:00' as timestamp);
+
+select cast('true' as boolean),
+       cast('false' as boolean),
+       cast('T' as boolean),
+       cast('F' as boolean);
+
+select '10'::integer, '2020-01-01'::date, '2020-01-01 12:00:00'::timestamp, 'true'::boolean;;
